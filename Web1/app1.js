8497fe3b549438d771f23559685a7ac881917cba
Go_Do_Yeon <112530476+brownanchovy@users.noreply.github.com>
2022-10-14 10:49:02 +0900

    10/14

--- Web1/app1.js
+++ Web1/app1.js
@@ -1,1 +1,23 @@
 const loginForm = document.querySelector('#login-form');
+const loginInput = loginForm.querySelector("input");
+//const loginButton = loginForm.querySelector("button");
+
+function onLoginClick() {
+  const username = loginInput.value
+  console.log(username);
+/*
+  if (username === "") {
+    alert("Please write your name");
+  }
+  else if (username.length >= 15) {
+    alert("Your name is too long")
+  }
+*/
+}
+
+function onLoginSubmit() {
+  const username = loginInput.value
+  console.log(username);
+}
+//loginButton.addEventListener("click", onLoginClick);
+loginForm.addEventListener("submit", onLoginSubmit);
