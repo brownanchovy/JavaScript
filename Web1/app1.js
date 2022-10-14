c78f8b9d6f2e5213328d530e9f8c038343e7f692
Go_Do_Yeon <112530476+brownanchovy@users.noreply.github.com>
2022-10-14 05:03:34 -0400

    10/14

--- Web1/app1.js
+++ Web1/app1.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector("input");
 //const loginButton = loginForm.querySelector("button");
+const link = document.querySelector("a");
+const greeting = document.querySelector('#greeting');
 
 function onLoginClick() {
   const username = loginInput.value
@@ -15,9 +17,22 @@ function onLoginClick() {
 */
 }
 
-function onLoginSubmit() {
-  const username = loginInput.value
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add("hidden");
+  //below two are same
+  greeting.innerText = "Hello " + username;
+  greeting.innerText = 'Hello ${username}'';
+  greeting.classList.remove("hidden");
 }
+
+function handleLinkClick(info) {
+  console.log(info);
+  event.preventDefault();
+  alert("click!!!");
+}
+
 //loginButton.addEventListener("click", onLoginClick);
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
